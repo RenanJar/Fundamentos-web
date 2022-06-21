5c8dc912888cb1975a4687080ad9e08b185dfa2d
PyRenan <Renan.olive.nasc@gmail.com>
2022-06-21 11:09:24 -0300

    HTML5

---
+++ web-fundamentos/assets/js/script.js
@@ -0,0 +1,19 @@
+/*
+Case Sensitive = reconhece letras maiusculas e minusulas
+
+por tag: getElementByTagName()
+por id: getElementById()
+por nome: getrElementesByName()
+por classe: getElementsByClassName()
+por seletor:querySelector()
+*/
+
+
+
+
+function validartudo(){
+    let nome = document.getElementById("nome").value;
+    let assunto =documento.getElementById("email").value;
+
+    alert(nome+" "+assunto);
+}
